50442f4bbe16ea01ceb5125c19618aad40140d76
shahiruni <shahiruni@a9c4ac11-bb59-4888-a949-8c1c6fc09797>
2012-09-13 04:13:41 +0000

    Bug #13972 : Daily Operation Log> Please put the correct source fault: TM,Celcom,Conty,Studio,OB, MCAR, STL

---
+++ joget/eamms_srcFault_txm.sql
@@ -0,0 +1,15 @@
+
+
+
+SELECT * FROM app_fd_eamms_srcFault_txm
+
+
+insert into app_fd_eamms_srcFault_txm (id,name,description,active)
+select 'TM','TM','TM','1' union all 
+select 'Celcom','Celcom','Celcom','1' union all 
+select 'Conty','Conty','Conty','1' union all 
+select 'Studio','Studio','Studio','1' 
+select 'OB','OB','StOBudio','1' 
+select 'MCAR','MCAR','MCAR','1' 
+select 'STL','STL','STL','1' 
+
